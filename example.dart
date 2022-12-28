10d0a72dad3ddd4a99fc5fc9ec746ba87fb39b72
Shadmehr <61777889+RexGod@users.noreply.github.com>
2022-12-28 23:08:49 +0330

    Create example.dart

---
+++ example.dart
@@ -0,0 +1,11 @@
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
